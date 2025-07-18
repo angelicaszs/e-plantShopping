ccf339fad63cbfa87cad67d8e422c3e1037c2440
angelicaszs <angelica.suarez@awaze.com>
2025-07-18 14:04:50 +0200

    removed unused variable

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 
 function ProductList({ onHomeClick }) {
   const [showCart, setShowCart] = useState(false);
-  const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+  const [, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 
   const [addedToCart, setAddedToCart] = useState({});
 
